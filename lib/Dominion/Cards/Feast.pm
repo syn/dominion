55cd629e252783a569ec5d4e120bc9aa7118a433
Alex Lawn <alex@catalyst-au.net>
2010-08-15 09:45:02 +1000

    Implement Feast

--- lib/Dominion/Cards/Feast.pm
+++ lib/Dominion/Cards/Feast.pm
@@ -10,6 +10,12 @@ sub cost_coin   { 4 }
 sub cost_potion { 0 }
 
 # Trash this card. Gain a card costing up to 5 Gold.
+sub action {
+    my ($self, $player, $game) = @_;
+    my @cards = grep { $_->cost_coin <= 5 } $game->supply->cards;
+    $game->trash->add($self);
+	$game->interaction_add(Dominion::Interaction::FreeBuy->new(player => $player, cards => [@cards], callback => sub {},message => 'Feast: Gain a card costing up to 5.'));
+}
 
 #__PACKAGE__->meta->make_immutable;
 1;
