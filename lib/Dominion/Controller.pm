8d7754f6e156f3bbf0514c1ba8ff6abe4a8ccf00
Alex Lawn <alex@catalyst-au.net>
2010-08-15 20:11:02 +1000

    Turns out shuffle messages are just plain anoying

--- lib/Dominion/Controller.pm
+++ lib/Dominion/Controller.pm
@@ -46,10 +46,10 @@ has 'player' => (
 			my ($p, $card) = @_;
 		   	$p->game->send_to_everyone_else(Dominion::Com::Messages::CardPlayed->new(actiontype => 'Discard', card=>$card, player=>$p),$p);
 		});	
-		$player->add_listener('deckshuffled', sub {
-			my ($p) = @_;
-			$p->game->send_to_everyone(Dominion::Com::Messages::Chat->new(section => 'game', message => $p->name . "'s deck shuffle." , from => 'System'));
-		});			
+		#$player->add_listener('deckshuffled', sub {
+			#my ($p) = @_;
+			#$p->game->send_to_everyone(Dominion::Com::Messages::Chat->new(section => 'game', message => $p->name . "'s deck shuffle." , from => 'System'));
+		#});			
         
 
         foreach my $cb ( keys %{$self->curried_callbacks} ) {
